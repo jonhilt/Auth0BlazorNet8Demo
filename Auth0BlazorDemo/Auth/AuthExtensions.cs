88b4b310875fe7310a23b8918c3f7d16fd5bb79e
Jon Hilton <hilton.jon@gmail.com>
2024-01-23 10:46:36 +0000

    Initial commit

---
+++ Auth0BlazorDemo/Auth/AuthExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Auth0BlazorDemo.Auth;
+
+public static class AuthExtensions
+{
+    public static void MapAuthRoutes(this WebApplication app)
+    {
+        app.MapGet("/Account/Login", async (HttpContext httpContext, string returnUrl = "/") =>
+        {
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                .WithRedirectUri(returnUrl)
+                .Build();
+
+            authenticationProperties.IsPersistent = true;
+
+            await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        });
+
+        app.MapPost("/Account/LogOut", async (HttpContext httpContext) =>
+        {
+            await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme);
+            await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        });
+    }
+}
---
+++ Auth0BlazorDemo/Program.cs
@@ -0,0 +1,48 @@
+using Auth0.AspNetCore.Authentication;
+using Auth0BlazorDemo.Auth;
+using Auth0BlazorDemo.Client.Pages;
+using Auth0BlazorDemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services
+    .AddAuth0WebAppAuthentication(options =>
+    {
+        options.Domain = builder.Configuration["Auth0:Domain"]!;
+        options.ClientId = builder.Configuration["Auth0:AppClientId"]!;
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapAuthRoutes();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
